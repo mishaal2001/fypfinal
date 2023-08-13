8a37da7d76549b8ad4457e6654d9b5ef001f70ae
mishaal2001 <140327581+mishaal2001@users.noreply.github.com>
2023-08-13 21:50:26 +0500

    Update app16.py

--- app16.py
+++ app16.py
@@ -14,7 +14,7 @@ import ctypes.util
 import ctypes
 
 # Load the libsndfile.dll library
-libsndfile = ctypes.CDLL('./libs/libsndfile.dll')  # Provide the correct path
+libsndfile = ctypes.CDLL('./libsndfile/bin/sndfile.dll')  # Provide the correct path
 
 
 import os
